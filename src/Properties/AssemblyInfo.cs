316946a67073a9e7506b9d49a2ebd7bc07709a50
Brendan Connolly <brendan@Brendans-MacBook-Pro.local>
2016-11-27 11:50:01 -0800

    move files into src folder

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Xunit.Categories")]
+[assembly: AssemblyTitle("unit.Categories")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
