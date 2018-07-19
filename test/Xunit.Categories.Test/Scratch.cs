5caf359cef3a7cf95392a1bad9e411ebea9ea2b7
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-07-19 10:46:16 +0200

    clean up and untabify

--- test/Xunit.Categories.Test/Scratch.cs
+++ test/Xunit.Categories.Test/Scratch.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using Xunit;
-using Xunit.Categories;
 
 namespace Xunit.Categories.Test
 {
     public class Scratch
     {
-
         [Fact]
         public void Test()
         {
@@ -31,53 +28,52 @@ namespace Xunit.Categories.Test
         [Fact, IntegrationTest]
         [Feature(888)]
         [Feature("888")]
-
         public void TestFeatureWithId()
         {
             throw new NotImplementedException("I've got your feature");
         }
 
-	    [Fact]
-		[KnownBug]
-	    public void TestKnownBug()
-	    {
-		    throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
-	    }
+        [Fact]
+        [KnownBug]
+        public void TestKnownBug()
+        {
+            throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
+        }
 
-	    [Fact]
-	    [KnownBug(666)]
-	    [KnownBug("666 a")]
-	    public void TestKnownBugWithId()
-	    {
-		    throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
-	    }
+        [Fact]
+        [KnownBug(666)]
+        [KnownBug("666 a")]
+        public void TestKnownBugWithId()
+        {
+            throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
+        }
 
-	    [Fact]
-	    [Documentation]
-	    [Documentation(666)]
-	    [Documentation("666 a")]
-	    public void TestDocumentation()
-	    {
-		    throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
-	    }
+        [Fact]
+        [Documentation]
+        [Documentation(666)]
+        [Documentation("666 a")]
+        public void TestDocumentation()
+        {
+            throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
+        }
 
-	    [Fact]
-	    [Exploratory]
-	    [Exploratory(666)]
-	    [Exploratory("666 a")]
-	    public void TestExploratory()
-	    {
-		    throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
-	    }
+        [Fact]
+        [Exploratory]
+        [Exploratory(666)]
+        [Exploratory("666 a")]
+        public void TestExploratory()
+        {
+            throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
+        }
 
-	    [Fact]
-	    [WorkItem]
-	    [WorkItem(666)]
-	    [WorkItem("666 a")]
-	    public void TestWorkItem()
-	    {
-		    throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
-	    }
+        [Fact]
+        [WorkItem]
+        [WorkItem(666)]
+        [WorkItem("666 a")]
+        public void TestWorkItem()
+        {
+            throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
+        }
 
         [Fact]
         [SystemTest]
@@ -87,11 +83,5 @@ namespace Xunit.Categories.Test
         {
             throw new NotImplementedException("request #8 - This is more black box, high level, and stress testing of a complete running application different to an IntegrationTest");
         }
-
-
-
-
-
     }
-
 }
