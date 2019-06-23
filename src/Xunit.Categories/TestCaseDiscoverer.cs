8d8177d498ac712d08fc1d6297d15f4c0d61850f
Alexandre Varizi Ribeiro <vribeiro.alexandre@gmail.com>
2019-06-23 17:07:24 -0300

    Adding 'TestCase' trait attribute (#13)
    thanks for contributing!

---
+++ src/Xunit.Categories/TestCaseDiscoverer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Xunit.Categories
+{
+    public class TestCaseDiscoverer : ITraitDiscoverer
+	{
+		internal const string DiscovererTypeName = DiscovererUtil.AssemblyName + "." + nameof(TestCaseDiscoverer);
+
+        /// <summary>
+        /// Gets the trait values from the trait attribute.
+        /// </summary>
+        /// <param name="traitAttribute"></param>
+        /// <returns></returns>
+		public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+		{
+			var testCaseId = traitAttribute.GetNamedArgument<string>("TestCaseId");
+
+
+			yield return new KeyValuePair<string, string>("Category", "TestCase");
+
+			if (!string.IsNullOrWhiteSpace(testCaseId))
+				yield return new KeyValuePair<string, string>("TestCase", testCaseId);
+
+		}
+	}
+}
