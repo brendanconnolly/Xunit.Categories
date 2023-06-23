c81969422b34a380ff5e40866e3e29a550a53be3
Adam Fisher <adam.fisher@octanesoftware.net>
2023-06-23 17:22:30 -0400

    Updated READMEAdd DatabaseTest, SnapshotTest, and Expensive categories.

---
+++ src/Xunit.Categories/SnapshotTestAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xunit.Sdk;
+
+namespace Xunit.Categories
+{
+    [TraitDiscoverer(IntegrationTestDiscoverer.DiscovererTypeName, DiscovererUtil.AssemblyName)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class SnapshotTestAttribute:Attribute, ITraitAttribute
+    {
+
+    }
+}
