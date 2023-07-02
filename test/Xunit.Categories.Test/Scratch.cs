dc407cd30134a72335beede78afefbf35f35394f
emragins <emragins@gmail.com>
2023-07-02 08:42:17 -0700

    Add Specification trait (#23)
    * create specification trait
    
    * rename to specification

--- test/Xunit.Categories.Test/Scratch.cs
+++ test/Xunit.Categories.Test/Scratch.cs
@@ -66,6 +66,16 @@ namespace Xunit.Categories.Test
             throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
         }
 
+
+        [Fact]
+        [Specification]
+        [Specification(666)]
+        [Specification("666 a")]
+        public void TestSpecification()
+        {
+            throw new NotImplementedException("I'm not that important, all the world knows about me, I'll be fixed in 2030");
+        }
+
         [Fact]
         [WorkItem]
         [WorkItem(666)]
